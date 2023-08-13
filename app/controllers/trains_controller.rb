37545210bb35f35f72e20c9f765871803af8fd4a
momomo0505 <techcamp.fujita.t@gmail.com>
2023-08-13 16:51:00 +0900

    出品機能作成

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -1,7 +1,9 @@
 class TrainsController < ApplicationController
   
   def index
+
     @train = Train.find_by(id: 1)
+    @sheets = @train.sheets.pluck(:seat_number)  # このトレインに関連するシートの席番号を配列として取得
     unless @train
       # 何らかの処理（例：エラーメッセージを表示してトップページへリダイレクトする）
       redirect_to root_path, alert: 'Train not found'
