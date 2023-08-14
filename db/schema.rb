92069ec0598ed0053ddf7c7d3fb839c6ff51fa41
momomo0505 <techcamp.fujita.t@gmail.com>
2023-08-14 20:04:46 +0900

    自分が出品中の席をクリックするとsheets/show.html.erbに移動

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_13_055212) do
+ActiveRecord::Schema.define(version: 2023_08_14_074930) do
+
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "sheet_id", null: false
+    t.integer "amount"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sheet_id"], name: "index_orders_on_sheet_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "sheets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -48,6 +59,8 @@ ActiveRecord::Schema.define(version: 2023_08_13_055212) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "sheets"
+  add_foreign_key "orders", "users"
   add_foreign_key "sheets", "users", column: "buyer_id"
   add_foreign_key "sheets", "users", column: "seller_id"
 end
