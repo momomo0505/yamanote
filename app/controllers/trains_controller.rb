5c4d54cfb24b986380efefba55c630b2cd797dc3
momomo0505 <techcamp.fujita.t@gmail.com>
2023-08-24 20:08:04 +0900

    全１１号車まで表示、出品、購入機能実装

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -1,13 +1,10 @@
 class TrainsController < ApplicationController
   
   def index
+    @number_of_trains = 11
+    @trains = Train.limit(@number_of_trains)
 
-    @train = Train.find_by(id: 1)
-    @sheets = @train.sheets.pluck(:seat_number)  # このトレインに関連するシートの席番号を配列として取得
-    unless @train
-      # 何らかの処理（例：エラーメッセージを表示してトップページへリダイレクトする）
-      redirect_to root_path, alert: 'Train not found'
-    end
+
   end
 
 end
