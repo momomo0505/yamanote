619067860ce083d3ec844f79a79dfbc896213098
momomo0505 <techcamp.fujita.t@gmail.com>
2025-11-01 13:25:05 +0900

    デプロイ

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,23 @@ require_relative 'boot'
 
 require 'rails/all'
 
+# Ruby 3.1+ compatibility: Enable YAML aliases for Psych 4.0+
+if Psych::VERSION >= '4.0'
+  module Psych
+    class << self
+      alias_method :load_without_aliases, :load
+
+      def load(yaml, *args)
+        if args.empty?
+          load_without_aliases(yaml, aliases: true)
+        else
+          load_without_aliases(yaml, *args)
+        end
+      end
+    end
+  end
+end
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,6 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+# Ruby 3.1+ compatibility: explicitly require logger
+require 'logger'
